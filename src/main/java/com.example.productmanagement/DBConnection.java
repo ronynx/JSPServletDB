90badcce145d898bd20af74af93ee15cb939bb60
ronynx <260001487+ronynx@users.noreply.github.com>
2026-07-23 23:29:43 +0545

    Add DBConnection class for MySQL database connection

---
+++ src/main/java/com.example.productmanagement/DBConnection.java
@@ -0,0 +1,29 @@
+package com.example.productmanagement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/productdb";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+
+        Connection con = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+}
